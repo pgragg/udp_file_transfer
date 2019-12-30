e26f7736cf79d7f29af621a5b7b836a5a37ec445
Piper Gragg <pipergragg@gmail.com>
2019-12-29 23:08:53 -0500

    WIP

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,8 @@ var transferFile_1 = require("./modules/fileTransfer/transferFile");
 if (!module.parent) {
     transferFile_1.transferFile({
         port: 2222,
-        fileName: './src/files/smol.txt',
-        targetFileName: './src/files/smol2.txt'
+        fileName: './src/files/typescript.pdf',
+        targetFileName: './src/files/typescript2.pdf'
     }).then(function () {
         console.log("Done");
     });
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { transferFile } from './modules/fileTransfer/transferFile'
 if (!module.parent) {
   transferFile({
     port: 2222,
-    fileName: './src/files/smol.txt',
-    targetFileName: './src/files/smol2.txt'
+    fileName: './src/files/typescript.pdf',
+    targetFileName: './src/files/typescript2.pdf'
   }).then(() => {
     console.log("Done")
   })
